30e483a5d6756b23ed7e2949c046bb2ad96b9ee7
pikachutrainer <ng.philip.91@gmail.com>
2023-03-06 15:14:06 +0100

    add pokeapi

---
+++ src/api/pokeapi.ts
@@ -0,0 +1,31 @@
+export interface Pokemon {
+    name: string;
+    order: number;
+    sprites: {
+        front_default: string;
+        back_default: string;
+    };
+    types: PokemonType[]
+}
+
+export interface PokemonType {
+    slot: number;
+    type: {
+        name: string
+    }
+}
+
+const headers = { 'Content-type': 'application/json' };
+const endpoint = 'https://pokeapi.co/api/v2/pokemon/';
+
+export function fetchPokemonByIndex(index: number): Promise<Pokemon> {
+    return fetch(`${endpoint}${index}?variables=name`, {
+        headers,
+      }).then(res => res.json());
+}
+
+export function fetchPokemonByName(name: string): Promise<Pokemon> {
+    return fetch(`${endpoint}${name}`, {
+        headers,
+      }).then(res => res.json());
+}
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,17 @@
 <script setup lang="ts">
 import WelcomeItem from '../components/WelcomeItem.vue'
+
+import { fetchPokemonByIndex, type Pokemon } from '@/api/pokeapi';
+import { onMounted } from 'vue';
+
+
+onMounted(() => {
+  loadData();
+});
+
+function loadData() {
+  fetchPokemonByIndex(3).then((pkmn: Pokemon) => console.log(pkmn));
+}
 </script>
 
 <template>
